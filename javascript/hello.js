a71a67e42739d128503e13795ef701ff424c3a95
Philip Chimento <philip.chimento@gmail.com>
2017-06-19 20:26:56 -0700

    js: Switch to 4-space indent
    This is the indentation style used in GNOME Shell, GJS, and other apps.

--- javascript/hello.js
+++ javascript/hello.js
@@ -5,42 +5,42 @@ const Gio = imports.gi.Gio;
 const Lang = imports.lang;
 
 const HelloWindow = new Lang.Class({
-  Name: 'HelloWindow',
-  Extends: Gtk.Window,
+    Name: 'HelloWindow',
+    Extends: Gtk.Window,
 
-  _init: function(app) {
-    this.parent({ application: app,
-                  default_width: 600,
-                  default_height: 300 });
+    _init: function(app) {
+        this.parent({ application: app,
+                      default_width: 600,
+                      default_height: 300 });
 
-    let headerbar = new Gtk.HeaderBar({
-      'visible': true,
-      'show-close-button': true,
-      'title': 'Hello, World!'
-    });
-    this.set_titlebar(headerbar);
+        let headerbar = new Gtk.HeaderBar({
+            'visible': true,
+            'show-close-button': true,
+            'title': 'Hello, World!'
+        });
+        this.set_titlebar(headerbar);
 
-    let label = new Gtk.Label({
-      'visible': true,
-      'use-markup': true,
-      'label': '<span size="larger" weight="bold">Hello, World!</span>'
-    });
-    this.add(label);
-  }
+        let label = new Gtk.Label({
+            'visible': true,
+            'use-markup': true,
+            'label': '<span size="larger" weight="bold">Hello, World!</span>'
+        });
+        this.add(label);
+    }
 });
 
 let app = new Gtk.Application({
-  'application-id': 'com.example.Hello',
-  'flags': Gio.ApplicationFlags.FLAGS_NONE
+    'application-id': 'com.example.Hello',
+    'flags': Gio.ApplicationFlags.FLAGS_NONE
 });
 
 app.connect('activate', function(app, event) {
-  let win = app.get_active_window();
+    let win = app.get_active_window();
 
-  if (win == null)
-    win = new HelloWindow(app);
+    if (win == null)
+        win = new HelloWindow(app);
 
-  win.present();
+    win.present();
 });
 
 app.run(null);
