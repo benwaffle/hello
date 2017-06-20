9cd22b1b2ea165ad576e671f390ebd1c44aee9c4
Philip Chimento <philip.chimento@gmail.com>
2017-06-19 20:54:59 -0700

    js: Use trailing commas
    They are in use in the GJS style guide, we recommend them because they
    make smaller diffs.

--- javascript/hello.js
+++ javascript/hello.js
@@ -12,28 +12,28 @@ const HelloWindow = new Lang.Class({
         this.parent({
             application: app,
             default_width: 600,
-            default_height: 300
+            default_height: 300,
         });
 
         let headerbar = new Gtk.HeaderBar({
             visible: true,
             show_close_button: true,
-            title: 'Hello, World!'
+            title: 'Hello, World!',
         });
         this.set_titlebar(headerbar);
 
         let label = new Gtk.Label({
             visible: true,
             use_markup: true,
-            label: '<span size="larger" weight="bold">Hello, World!</span>'
+            label: '<span size="larger" weight="bold">Hello, World!</span>',
         });
         this.add(label);
-    }
+    },
 });
 
 let app = new Gtk.Application({
     application_id: 'com.example.Hello',
-    flags: Gio.ApplicationFlags.FLAGS_NONE
+    flags: Gio.ApplicationFlags.FLAGS_NONE,
 });
 
 app.connect('activate', function(app, event) {
